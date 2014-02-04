93f056e93df055f7ce6012c5c16001fb31defc4a
VardanGrigoryan <spec_master_arm@mail.ru>
2014-02-04 22:44:16 +0400

    second commit

--- lex.py
+++ lex.py
@@ -11,9 +11,9 @@ class Lexer(object):
 	_RESISTOR_PATTERN = '(^[r|R][0-9a-zA-Z_]+)'
 	_CAPACITOR_PATTERN = '(^[c|C][0-9a-zA-Z_]+)'
 	_DIODE_PATTERN = '(^[d|D][0-9a-zA-Z_]+)'
-	_PIN_PATTERN = '(^[a-zA-z]+[0-9]+[:][a-zA-Z0-9]+)'
+	_PIN_PATTERN = '(^[a-zA-z]+[0-9]*[:]?[a-zA-Z0-9]*)'
 	_ATTRIBUTE_PATTERN = '(^[a-zA-z_]+[0-9]+(\s)*[=](\s)*[a-z0-9.-]+)'
-	_VALUE = '(^[0-9a-z.]+)'
+	_VALUE_PATTERN = '(^[0-9a-z.]+)'
 	_NEW_LINE_PATTERN = '(^[+ ]+)'
 	def _string_to_list(self, s):
 		l = []
@@ -35,7 +35,30 @@ class Lexer(object):
 	#		line = buf.readline()
 		return line
 	#	return s
-#	def _check_attribute(self, s):
+	def _is_attribute(self, s):
+		a = re.match(self._ATTRIBUTE_PATTERN, s)
+		if a:
+			print True
+			return True
+		else:
+			print False
+			return False
+	def _is_pin(self, s):
+		a = re.match(self._PIN_PATTERN, s)
+		if a:
+			print True
+			return True
+		else:
+			print False
+			return False
+	def _is_value(self, s):
+		a = re.match(self._VALUE_PATTERN, s)
+		if a:
+			print True
+			return True
+		else:
+			print False
+			return False
 	def _check_element(self, s):
 		ss = self._clear_line(s)
 		t = re.match(self._TRANSISTOR_PATTERN, ss)
@@ -50,10 +73,11 @@ class Lexer(object):
 				print s
 				return "TRANSISTOR"
 			if r:
+				l = self._string_to_list(s)
+				self._make_tokens(l)
 				return "RESISTOR"
 			if c:
 				l = self._string_to_list(s)
-		#		print l[0]
 				self._make_tokens(l)
 				return "CAPACITOR"
 			if d:
@@ -61,7 +85,33 @@ class Lexer(object):
 			if n:
 				return "NEW LINE"
 	def _make_tokens(self, l):
+		ll = []
+		k = 0
 		for i in xrange(len(l)):
-			print l[i]
+			k = k + 1
+			print k
+			ba = self._is_attribute(l[i])
+			bp = self._is_pin(l[i])
+			bv = self._is_value(l[i])
+#			if not ba and not bp and not bv:
+#				return "Unrecognizable instructions"
+#			else:
+			if ba:
+				t = (self._ATTRIBUTE, l[i])
+				print t
+				ll.append(t)
+			if bp:
+				t = (self._PIN, l[i])
+				print t
+				ll.append(t)
+			if ba:
+				t = (self._VALUE, l[i])
+				print t
+				ll.append(t)
+		if not ll:
+			return ll
+		else:
+			return None
+
 
 
