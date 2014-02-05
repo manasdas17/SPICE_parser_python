0b28f5b8a0c3a3b40dabe4cd02be3a314659c16e
VardanGrigoryan <spec_master_arm@mail.ru>
2014-02-05 14:54:35 +0400

    corrected pin and atributes presentation

--- lex.py
+++ lex.py
@@ -12,35 +12,16 @@ class Lexer(object):
 	_CAPACITOR_PATTERN = '(^[c|C][0-9a-zA-Z_]+)'
 	_DIODE_PATTERN = '(^[d|D][0-9a-zA-Z_]+)'
 	_ELEMENT_PATTERN = '(^[a-zA-z]+[a-zA-Z0-9]*)'
-	_PIN_PATTERN = '(^[a-zA-z]+[0-9]*[:]?[a-zA-Z0-9]*)'
-	_ATTRIBUTE_PATTERN = '(^[a-zA-z_]+[0-9]+(\s)*[=](\s)*[a-z0-9.-]+)'
-	_VALUE_PATTERN = '(^[0-9a-z.+-]+)'
+#	_PIN_PATTERN = '(^[a-zA-z]+[0-9]*[:]?[a-zA-Z0-9]*)'
+#	_ATTRIBUTE_PATTERN = '(^[a-zA-Z0-9_]+[0-9]*(\s)*[=](\s)*[a-z0-9.-]+)'
+	_ATTRIBUTE_PATTERN = r'[\w_]+\s*=\s*[\w\.\-]+' # matches ad = 0.013p
+	_PIN_PATTERN = r'\w+\s*:?\s*\w+'  # matches VSS:F85
+	_VALUE_PATTERN = '(^[0-9eu.+-]+)'
 	_NEW_LINE_PATTERN = '(^[+ ]+)'
 	def _string_to_list(self, s):
 		l = []
 		l = s.split(" ")
 		return l
-	def _clear_line(self, s):
-		if s.startswith ("\xef\xbb\xbf") :
-			print s
-			s = s.rstrip("\xef\xbb\xbf")
-			l = s.split()
-			print l
-		if s.endswith("\r\n") :
-			print s
-			s = s.rstrip("\r\n")
-			l = s.split()
-			print l
-		return s
-#	def _clear_line(self, s):
-#		tt = s.split()
-#		ss = ''.join(tt[0])
-#		if ss.startswith ("\xef\xbb\xbf") :
-#			t = ss.split("\xef\xbb\xbf")
-#			ss = ''.join(t)
-#			tt[0] = ss
-#			return ss
-#		return ss
 	def _parse_one_line(self, s):
 		buf = StringIO.StringIO(s)
 		line = buf.readline()
@@ -62,7 +43,8 @@ class Lexer(object):
 			return False
 	def _is_pin(self, s):
 		a = re.match(self._PIN_PATTERN, s)
-		if a:
+		e = re.match('([^=]+=[^=]+)', s)
+		if a and e is None:
 			return True
 		else:
 			return False
@@ -73,7 +55,6 @@ class Lexer(object):
 		else:
 			return False
 	def _check_element(self, s):
-#		ss = self._clear_line(s)
 		ss = s
 		t = re.match(self._TRANSISTOR_PATTERN, ss)
 		r = re.match(self._RESISTOR_PATTERN, ss)
@@ -84,7 +65,8 @@ class Lexer(object):
 			return "Unrecognizable instructions"
 		else:
 			if t:
-				print s
+				l = self._string_to_list(s)
+				self._make_tokens(l)
 				return "TRANSISTOR"
 			if r:
 				l = self._string_to_list(s)
@@ -101,15 +83,11 @@ class Lexer(object):
 				return "NEW LINE"
 	def _make_tokens(self, l):
 		ll = []
-		k = 0
 		for i in xrange(len(l)):
 			ba = self._is_attribute(l[i])
 			be = self._is_element(l[i])
 			bp = self._is_pin(l[i])
 			bv = self._is_value(l[i])
-#			if not ba and not bp and not bv:
-#				return "Unrecognizable instructions"
-#			else:
 			if ba:
 				t = (self._ATTRIBUTE, l[i])
 				print t
